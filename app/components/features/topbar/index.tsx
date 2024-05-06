dcde945f93ccbb66c0e1b1d2b631ee20982ad28a
Kaynã de Camargo <kayna.khad@gmail.com>
2024-05-05 21:04:27 -0300

    Fix: Chat overlapping topbar

--- app/components/features/topbar/index.tsx
+++ app/components/features/topbar/index.tsx
@@ -1,6 +1,6 @@
 const Topbar = () => {
   return (
-    <div className="w-full flex justify-center items-center h-[40px] absolute top-0 border-l-0 border-r-0 border-t-0 border-dotted border-secondary shadow-secondary">
+    <div className="w-[70%] bg-background z-10 flex justify-center items-center h-[40px] absolute top-0 border-l-0 border-r-0 border-t-0 border-dotted border-secondary shadow-secondary">
       <div className="cursor-pointer flex flex-col items-center space-x-2 w-fit">
         <span className="text-sm font-bold">Gyoza OS</span>
         <span className="text-xs">
